dc80bbf84d6479a51c469665e5686906e66f6ba5
Sasha Shkapenko <sasha.shkapenko@gmail.com>
2022-07-05 14:07:15 +0100

    adds contacts to local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,10 @@ import ContactList from "./ContactList";
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   }
@@ -49,6 +49,17 @@ class App extends Component {
     const { contacts, filter } = this.state;
     const normalizedFilter = filter.toLocaleLowerCase();
     return contacts.filter(contact => contact.name.toLocaleLowerCase().includes(normalizedFilter));
+  }
+  componentDidMount = () => {
+    const contactList = JSON.parse(localStorage.getItem('contacts'));
+    if (contactList) {
+      this.setState({ contacts: contactList })
+    }
+  }
+  componentDidUpdate = (prevProps, prevState) => {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
   }
 
   render() {
